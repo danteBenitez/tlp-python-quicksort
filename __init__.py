fcd0bf3f1e5ed282c0ce0313eeac6a701f1dc056
danteBenitez <benitezdante123@gmail.com>
2024-05-25 10:15:33 -0300

    docs: Agregar comentarios, mejorar tipado, pruebas

--- __init__.py
+++ __init__.py
@@ -1,11 +1,31 @@
-def swap(arr, from_index, to_index):
-    [arr[from_index], arr[to_index]] = [arr[to_index], arr[from_index]]
-    return arr
+def quicksort(number_list: list[int | float]) -> list[int | float]:
+    """
+        Utiliza el algoritmo de quicksort para ordenar una lista de números
+        *in_place* (sin utilizar memoria adicional).
 
-def quicksort(number_list: list[int]) -> list[int]:
+        :param number_list: lista de números a ordenar
+        :type list[int | float]
+
+        :return: lista de números ordenados
+        :rtype: list[int | float]
+    """
     return quicksort_impl(number_list, 0, len(number_list))
 
-def quicksort_impl(number_list: list[int], start: int, end: int) -> list[int]:
+def quicksort_impl(number_list: list[int | float], start: int, end: int) -> list[int | float]:
+    """
+        Implementación recursiva del algoritmo de quicksort
+        que ordena una sublista de `number_list` dado un rango
+        de índices [start, end). El ordenamiento es *in_place*.
+
+        :param number_list: lista de números a ordenar
+        :type list[int | float]
+
+        :param start: índice de inicio de la sublista
+        :type int
+
+        :param end: índice de fin de la sublista
+        :type int
+    """
     length = end - start + 1
     if length <= 1:
         return number_list
@@ -28,8 +48,8 @@ def quicksort_impl(number_list: list[int], start: int, end: int) -> list[int]:
     quicksort_impl(number_list, pivot_index+1, end)
     return number_list
 
-    # print(number_list)
-print(quicksort([1, 2, 3, 4, -10, -20]))
-print(quicksort([5,4, 6, 8, 9, 1, 3, 10, -2, -4]))
-# print(quicksort([0, 1, -1, 2]))
-# quicksort([-10, -2, 10, 30, -5, -11])
+assert quicksort([1, 2, 3, 4, -10, -20]) == [-20, -10, 1, 2, 3, 4]
+assert quicksort([5,4, 6, 8, 9, 1, 3, 10, -2, -4] == [-4, -2, 1, 3, 4, 5, 6, 8, 9, 10])
+assert quicksort([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5] 
+
+print("¡Pruebas pasadas exitosamente!")
